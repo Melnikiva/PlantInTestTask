240c032771d22f162c1a08869ecb2b1f2a38da63
Ivan Melnyk <ivanmel.vn23@gmail.com>
2022-06-05 13:03:09 +0300

    Initial Commit

---
+++ PlantInTestTask/PlantInTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantInTestTaskApp.swift
+//  PlantInTestTask
+//
+//  Created by Ivan Melnyk on 05.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PlantInTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
